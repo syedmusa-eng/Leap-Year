4fd86aee5a30e3ef20e56d046bbd6106d59e0783
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-04-28 01:45:57 +0200

    TestApp to check the leap year

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Calling the mathod to check the leap year
+        Boolean Year = LeapYear.isLeapYear(2000);
+        System.out.println(Year);
+    }
+}
